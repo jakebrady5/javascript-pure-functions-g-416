5b1da7891822482ff150c8c3eb971930e2929167
Jake Brady <jake@cmdrconsulting.com>
2016-12-07 14:50:19 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const phrase = " Tada!";
+
+const addCatchPhrase = (app, phrase) => app.concat(phrase);
+
+console.log(addCatchPhrase(app, phrase));
+console.log(app);
